3848edea40891c000db91f479974bfc12f7d4ea5
Stuart Smith <ssmith_1975@yahoo.com>
2017-04-08 14:00:48 -0700

    moving Rest service to its own folder

---
+++ Rasberry-Pi-Trebuchet/Azure-WebService/Raspberry_Pi_Trebuchet.Azure-RestWebService/Services/AzureIOTRequestResponceService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Raspberry_Pi_Trebuchet.Azure_WebService.RestViewModel;
+
+namespace Raspberry_Pi_Trebuchet.Azure_WebService.Services
+{
+    public class AzureIOTRequestResponceService
+    {
+        private static AzureIOTRequestResponceService _instance;
+
+        private ConcurrentDictionary<Guid, rvm_IOTAzureRequestResponce> requestedConcurrentDictionary;
+
+        private AzureIOTRequestResponceService()
+        {
+            requestedConcurrentDictionary = new ConcurrentDictionary<Guid, rvm_IOTAzureRequestResponce>();
+        }
+
+
+        public void Add(rvm_IOTAzureRequestResponce msgContentToAndFromAzure)
+        {
+            requestedConcurrentDictionary.TryAdd(msgContentToAndFromAzure.MSGGUID, msgContentToAndFromAzure);
+        }
+
+
+        public rvm_IOTAzureRequestResponce Get(Guid guid)
+        {
+            rvm_IOTAzureRequestResponce msgContentToAndFromAzure;
+
+            if (requestedConcurrentDictionary.TryGetValue(guid, out msgContentToAndFromAzure))
+                return msgContentToAndFromAzure;
+
+            return null;
+        }
+
+        public void Delete(Guid guid)
+        {
+            rvm_IOTAzureRequestResponce msgContentToAndFromAzure;
+            requestedConcurrentDictionary.TryRemove(guid,out msgContentToAndFromAzure);
+        }
+
+        public static AzureIOTRequestResponceService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AzureIOTRequestResponceService();
+                }
+                return _instance;
+            }
+        }
+       
+    }
+}
